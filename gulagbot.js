e2e5db1696b147881e33f881ddf37de477f6f7be
batfolx <victorvelea007@yahoo.com>
2021-09-09 23:16:46 -0400

    Add error handling and timeout to removing people from Gulag

--- gulagbot.js
+++ gulagbot.js
@@ -5,7 +5,12 @@ const USER = process.env.ROCKETCHAT_USER;
 const PASS = process.env.ROCKETCHAT_PASSWORD;
 const SSL = false;  // server uses https ?
 const ROOMS = ['gulag', 'general'];
+let TIMEOUT = process.env.TIMEOUT;
+if (TIMEOUT === undefined || TIMEOUT === null) {
+    TIMEOUT = (1000 * 60 * 1); // 1s * 60sec in minute * 1 minutes timeout time
+}
 
+console.log('TIMEOUT', TIMEOUT);
 let myuserid;
 let gulagRoomId;
 
@@ -23,47 +28,52 @@ const gulagbot = async () => {
 // callback for incoming messages filter and processing
 const processMessages = async(err, message, messageOptions) => {
     if (!err) {
-        // filter our own message
-        if (message.u._id === myuserid) return;
+        try {
+            // filter our own message
+            if (message.u._id === myuserid) return;
 
-        const roomId = message._id;
-        // can filter further based on message.rid
-        const roomName = messageOptions.roomName;
-        const messageContent = message.msg;
-        const userId = message.u._id;
-        if (messageContent.startsWith("!gulag")) {
+            const roomId = message._id;
+            // can filter further based on message.rid
+            const roomName = messageOptions.roomName;
+            const messageContent = message.msg;
+            const userId = message.u._id;
+            if (messageContent.startsWith("!gulag")) {
 
 
-            // could possibly cache this, but for right now this'll do
-            const adminIds = await getAdmins();
-            const admins = adminIds['admins'];
-            const ids = adminIds['ids'];
-            // don't listen to anybody who is not an admin
-            if (!ids.includes(userId)) {
-                await driver.sendToRoom('You cannot send someone to the Gulag. You are not an Admin!', roomName);
-                return;
+                // could possibly cache this, but for right now this'll do
+                const adminIds = await getAdmins();
+                const admins = adminIds['admins'];
+                const ids = adminIds['ids'];
+                // don't listen to anybody who is not an admin
+                if (!ids.includes(userId)) {
+                    await driver.sendToRoom('You cannot send someone to the Gulag. You are not an Admin!', roomName);
+                    return;
+                }
+
+                let mentions = message.mentions;
+                if (mentions.length === 0) {
+                    let response = 'You need to mention someone to send them to the Gulag.'
+                    await driver.sendToRoom(response, roomName);
+                } else {
+
+                    let response = "Sending ";
+                    let userNames = [];
+                    let userIds = [];
+                    mentions.forEach(function(mention) {
+                        let name = mention.username;
+                        response += name + ", "
+                        userNames.push(name);
+                        userIds.push(mention._id);
+                    });
+                    response += "to the Gulag.";
+                    await addUsersToTheGulag(gulagRoomId, userNames, userIds);
+                    await driver.sendToRoom(response, roomName);
+                }
             }
-
-            let mentions = message.mentions;
-            if (mentions.length === 0) {
-                let response = 'You need to mention someone to send them to the Gulag.'
-                await driver.sendToRoom(response, roomName);
-            } else {
-
-                let response = "Sending ";
-                let userNames = [];
-                let userIds = [];
-                mentions.forEach(function(mention) {
-                    let name = mention.username;
-                    response += name + ", "
-                    userNames.push(name);
-                    userIds.push(mention._id);
-                });
-                response += "to the Gulag.";
-                await addUsersToTheGulag(gulagRoomId, userNames, userIds);
-                await driver.sendToRoom(response, roomName);
-            }
+        } catch(e) {
+            console.log('Failed to process message with error', e.toString());
         }
+
     }
 }
 
@@ -121,12 +131,12 @@ async function addUsersToTheGulag(roomId, users, userIds) {
         }
         const response = await api.post('method.call/addUsersToRoom', request);
 
-        userIds.forEach(function (user) {
+        userIds.forEach(function (user, index) {
             setTimeout(function() {
-                removeUserFromGulag(roomId, user).catch(function(e) {
+                removeUserFromGulag(roomId, user, users[i]).catch(function(e) {
                     console.log('Error in removing', user, 'with error', e.toString());
                 });
-            }, 5000);
+            }, TIMEOUT);
         });
 
     } catch (e) {
@@ -134,10 +144,15 @@ async function addUsersToTheGulag(roomId, users, userIds) {
     }
 }
 
-async function removeUserFromGulag(roomId, userId) {
+async function removeUserFromGulag(roomId, userId, username) {
 
     let data = {roomId: roomId, userId: userId};
-    const response = await api.post('groups.kick', data);
+    try {
+        await api.post('groups.kick', data);
+    } catch (e) {
+        console.log('Failed to remove', username, 'from the gulag');
+    }
+
 
 }
 
